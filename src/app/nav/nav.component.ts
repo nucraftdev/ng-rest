ef25e857d79c1b4297c5280f22a322c1c108ae28
Scott Morey <github@bluei.net>
2019-10-06 20:01:58 -0400

    Before removing subscribe in auth service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -12,8 +12,6 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
   baseUrl = 'https://centralusdtpilot00.epicorsaas.com/SaaS506Third/';
-  // xml: string = '<Token xmlns="http://schemas.datacontract.org/2004/07/Epicor.Web" xmlns:i="http://www.w3.org/2001/XMLSchema-instance"><AccessToken>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxNTcwMzcyMDM1IiwiaWF0IjoiMTU3MDM2ODQzNSIsImlzcyI6ImVwaWNvciIsImF1ZCI6ImVwaWNvciIsInVzZXJuYW1lIjoiMTg4MjItc21vcmV5In0.E6lka7KCe4TTDlnVGB9Mf0nAbpt5iBh98VfULBmv7w4</AccessToken><ExpiresIn>3600</ExpiresIn><RefreshToken/><TokenType>Bearer</TokenType></Token>';
-  // jsonTest: any = {};
 
  constructor(private authService: AuthService, private xmlToJsonServiceService: XmlToJsonService) { }
 
@@ -24,8 +22,8 @@ export class NavComponent implements OnInit {
     // this.jsonTest = parseString(this.xml);
     // console.log(this.jsonTest);
     this.authService.login(this.model).subscribe(next => {
-      console.log('login SUCCESS');
-      // console.log(next);
+      console.log('login SUCCESS...');
+      console.log(next);
       // map(res => {
       //   console.log(xml2js.parseString(next)); // Prints JSON object!
       // })

dbc3c35fb4bfc903db143bee7525383586abe073
Nucraft Furniture <itmgr@nucraft.com>
2019-10-16 17:20:23 -0400

    Routing complete

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,11 +3,19 @@ import { HomeComponent } from './home/home.component';
 import { PerconListComponent } from './percon-list/percon-list.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { WorkforceListComponent } from './workforce-list/workforce-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'percon-list', component: PerconListComponent },
-    { path: 'user-list', component: UserListComponent },
-    { path: 'workforce-list', component: WorkforceListComponent },
+    { path: '', component: HomeComponent },  // route of nothing
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'percon-list', component: PerconListComponent },
+            { path: 'user-list', component: UserListComponent },
+            { path: 'workforce-list', component: WorkforceListComponent },
+        ]
+    },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
